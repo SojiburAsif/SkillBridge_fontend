d5f42b94d4879a66c42463234b7e91c796248884
Sojibur Rahman Asif <asif81534@gmail.com>
2026-02-03 15:53:50 +0600

    Batter auth Update

--- next.config.ts
+++ next.config.ts
@@ -12,11 +12,19 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/api/auth/:path*",
+  //       destination: `${process.env.NEXT_PUBLIC_API_URL}/api/auth/:path*`,
+  //     },
+  //   ];
+  // },
   async rewrites() {
     return [
       {
-        source: "/api/auth/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/auth/:path*`,
+        source: "/api/:path*",
+        destination: `https://skillbridgebackend.vercel.app/api/:path*`,
       },
     ];
   },
--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "https://skillbridgebackend.vercel.app/api/auth",
+    baseURL: "https://skill-bridge-fontend-five.vercel.app/api/auth",
     fetchOptions: {
         credentials: "include",
     },
